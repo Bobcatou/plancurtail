bc7932a845d0ea7e65693a3d9ea2024064b604f0
Bryan Cady <sw@listentothewindmedia.com>
2015-10-05 13:21:11 -0400

    Added CTA widget area

--- front-page.php
+++ front-page.php
@@ -9,6 +9,23 @@ remove_action( 'genesis_loop', 'genesis_do_loop' );
 remove_action( 'genesis_after_header', 'genesis_do_nav' );
 add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_nav' );
 
+//* Call to Action Widget Area
+add_action( 'genesis_before_content', 'lwm_cta', 15 );
+	function lwm_cta() {
+	echo '<div class="lwm_cta_block">';
+	echo '<div class="wrap cta_widgets">';
+		genesis_widget_area( 'lwm_cta_left', array(
+			'before' => '<div class="lwm_cta_leftside">',
+			'after' => '</div>',
+	) );
+			genesis_widget_area( 'lwm_cta_right', array(
+			'before' => '<div class="lwm_cta_rightside">',
+			'after' => '</div>',
+	) );
+	echo '</div>';
+	echo '</div>';  
+}
+
 
 
 //* Run the Genesis loop
