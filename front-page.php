dba71346b54a5fb4f081cc3cb2ad1b081689b9cd
Bryan Cady <sw@listentothewindmedia.com>
2015-10-05 20:08:09 -0400

    Removed
    //*Move Main Nav above image
    remove_action( 'genesis_after_header', 'genesis_do_nav' );
    add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_nav' );
    
    Line 8

--- front-page.php
+++ front-page.php
@@ -5,9 +5,6 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_c
 
 remove_action( 'genesis_loop', 'genesis_do_loop' );
 
-//*Move Main Nav above image
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_before_content_sidebar_wrap', 'genesis_do_nav' );
 
 //* Call to Action Widget Area
 add_action( 'genesis_before_content', 'lwm_cta', 15 );
