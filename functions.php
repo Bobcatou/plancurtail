bc2f4d38950a4340607f32aa14fbb744dbd902a9
Bryan Cady <sw@listentothewindmedia.com>
2015-10-05 21:32:04 -0400

    Added Search box text

--- functions.php
+++ functions.php
@@ -124,6 +124,17 @@ function howdy_message($translated_text, $text, $domain) {
 add_filter('gettext', 'howdy_message', 10, 3);
 
 
+
+//* Customize search form input box text
+add_filter( 'genesis_search_text', 'sp_search_text' );
+function sp_search_text( $text ) {
+	return esc_attr( 'Search Plancurtail' );
+}
+
+
+
+
+
 //* CTA Widget section
 
 //* 1st Column
