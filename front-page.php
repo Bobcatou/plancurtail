8ff5a094c88d4f6f87cd9a2f84127586bd41ceb5
Bryan Cady <sw@listentothewindmedia.com>
2015-10-13 20:21:42 -0400

    Changed CTA area with Columns

--- front-page.php
+++ front-page.php
@@ -24,11 +24,11 @@ add_action( 'genesis_before_content', 'lwm_cta', 15 );
 	echo '<div class="lwm_cta_block">';
 	echo '<div class="wrap cta_widgets">';
 		genesis_widget_area( 'lwm_cta_left', array(
-			'before' => '<div class="lwm_cta_leftside">',
+			'before' => '<div class="lwm_cta_left one-half first">',
 			'after' => '</div>',
 	) );
 			genesis_widget_area( 'lwm_cta_right', array(
-			'before' => '<div class="lwm_cta_rightside">',
+			'before' => '<div class="lwm_cta_right one-half">',
 			'after' => '</div>',
 	) );
 	echo '</div>';
