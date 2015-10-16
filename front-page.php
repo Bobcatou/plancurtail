1ee3d9d7095d3d2fd298c87efdcb7d94dc26e5f6
Bryan Cady <sw@listentothewindmedia.com>
2015-10-16 16:31:30 -0400

    rewrote widgets to make heading work

--- front-page.php
+++ front-page.php
@@ -20,34 +20,33 @@ remove_action( 'genesis_loop', 'genesis_do_loop' );
 
 
 
+
+
+
+
 //* CTA with description and Donate
 add_action( 'genesis_before_content', 'lwm_cta_donate', 15 );
-	function lwm_cta_donate() {
-	echo '<div class="lwm_cta_donate_area block">';
-	echo '<div class="wrap cta_donate_widgets">';
-	echo '<div class="slogan">';
-	echo '<h2>Put Phrase to Get Attention Here</h2>';
-			genesis_widget_area( 'lwm_donate_column_1', array(
-			'before' => '<div class="lwm_donate1 one-third first">',
-			'after' => '</div>',
-	) );
-			genesis_widget_area( 'lwm_donate_column_2', array(
-			'before' => '<div class="lwm_donate2 one-third">',
-			'after' => '</div>',
-	) );
-	echo '</div>';  
-			genesis_widget_area( 'lwm_donate_column_3', array(
-			'before' => '<div class="lwm_donate3 one-third">',
-			'after' => '</div>',
-	) );
-	echo '</div>';
-	echo '</div>';  
+ function lwm_cta_donate() {
+ echo '<div class="lwm_cta_donate_area block wrap">';
+ echo '<div class="slogan two-thirds first">';
+ echo '<h2>Put Phrase to Get Attention Here</h2>';
+   genesis_widget_area( 'lwm_donate_column_1', array(
+   'before' => '<div class="lwm_donate1 one-half first">',
+   'after' => '</div>',
+ ) );
+   genesis_widget_area( 'lwm_donate_column_2', array(
+   'before' => '<div class="lwm_donate2 one-half">',
+   'after' => '</div>',
+ ) );
+ echo '</div>';
+ echo '<div class="lwm_last_widget one-third">';  
+   genesis_widget_area( 'lwm_donate_column_3', array(
+   'before' => '<div class="lwm_donate3">',
+   'after' => '</div>',
+ ) );
+ echo '</div>';
+ echo '</div>';  
 }
-
-
-
-
-
 
 
 
