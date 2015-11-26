0b0a62c0d7e5730e63677874df4f4f9e049ed35f
Bryan Cady <sw@listentothewindmedia.com>
2015-11-26 08:07:06 -0500

    Made CO2 to <sub>2</sub> in title

--- front-page.php
+++ front-page.php
@@ -30,7 +30,7 @@ add_action( 'genesis_before_content', 'lwm_cta_donate', 15 );
  echo '<div class="lwm_cta_donate_area block wrap">';
  echo '<div class="slogan two-thirds first">';
  echo "<h2 class='survive'>IT'S A QUESTION OF SURVIVAL</h2>";
- echo "<h2>Annual C02 Emissions Worldwide - 80% of World Population</h2>";
+ echo "<h2>Annual C0<sub>2</sub> Emissions Worldwide - 80% of World Population</h2>";
    genesis_widget_area( 'lwm_donate_column_1', array(
    'before' => '<div class="lwm_donate1 one-half first">',
    'after' => '</div>',
