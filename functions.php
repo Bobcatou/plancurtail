30c23242dee2e36c46e490bf83140f76fcb80cf0
Bryan Cady <sw@listentothewindmedia.com>
2015-12-02 13:38:11 -0500

    Pulled CTA unused widget area

--- functions.php
+++ functions.php
@@ -284,17 +284,17 @@ genesis_register_sidebar( array(
 //* CTA Widget section
 
 //* 1st Column
-genesis_register_sidebar( array(
-	'id'            => 'lwm_cta_left',
-	'name'          => __( 'CTA - Left Side', 'plancurtail' ),
-	'description'   => __( 'Initial Build Coded 1/2 width', 'plancurtail' ),
-) );
+//genesis_register_sidebar( array(
+//	'id'            => 'lwm_cta_left',
+//	'name'          => __( 'CTA - Left Side', 'plancurtail' ),
+//	'description'   => __( 'Initial Build Coded 1/2 width', 'plancurtail' ),
+//) );
 //* 2nd Column
-genesis_register_sidebar( array(
-	'id'            => 'lwm_cta_right',
-	'name'          => __( 'CTA - Right Side', 'plancurtail' ),
-	'description'   => __( 'Initial Build Coded 1/2 width', 'plancurtail' ),
-) );
+//genesis_register_sidebar( array(
+//	'id'            => 'lwm_cta_right',
+//	'name'          => __( 'CTA - Right Side', 'plancurtail' ),
+//	'description'   => __( 'Initial Build Coded 1/2 width', 'plancurtail' ),
+//) );
 
 
 
